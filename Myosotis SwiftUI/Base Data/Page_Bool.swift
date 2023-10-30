cd4c1431ec731170c735c51684628fd4dbfde6e1
Acan <xie.zeyu20@gmail.com>
2023-10-30 13:52:10 +0800

    .

--- Myosotis SwiftUI/Base Data/Page_Bool.swift
+++ Myosotis SwiftUI/Base Data/Page_Bool.swift
@@ -10,8 +10,26 @@ import SwiftUI
 
 struct Page_Bool: View {
     var body: some View {
-        VStack {
-            Text("test")
-        }
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading) {
+                Text("布尔值：Bool").font(.title).padding(.bottom).bold().foregroundColor(.blue)
+                Text("""
+                \(Text("变换值").bold().font(.system(.body, design: .default)))
+                var show: Bool = true
+                show.toggle()
+                print(show)
+                // false
+                show.toggle()
+                print(show)
+                // true
+                """).font(.system(.body, design: .monospaced)).padding(.bottom)
+                
+                Text("""
+                \(Text("?:").bold().font(.system(.body, design: .default)))
+                Text(show ? \"显示\" : \"隐藏\")
+                """).font(.system(.body, design: .monospaced)).padding(.bottom)
+                
+            }.frame(alignment: .leading)
+        }.padding()
     }
 }
--- Myosotis SwiftUI/Base Data/Page_Date.swift
+++ Myosotis SwiftUI/Base Data/Page_Date.swift
@@ -10,8 +10,37 @@ import SwiftUI
 
 struct Page_Date: View {
     var body: some View {
-        VStack {
-            Text("test")
-        }
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading) {
+                
+                
+                Text("Date").font(.title).padding(.bottom).bold().foregroundColor(.blue)
+                Text("""
+                \(Text("获得日期").bold().font(.system(.body, design: .default)))
+                let currentDate = Date()
+                """).font(.system(.body, design: .monospaced)).padding(.bottom)
+                Text("""
+                \(Text("日期转化成要求的字符串").bold().font(.system(.body, design: .default)))
+                // DataFormatter()
+                let dateformatter = DateFormatter()
+                // dateStyle
+                dateformatter.dateStyle = .full
+                // timeStyle
+                dateformatter.timeStyle = .long
+                dateformatter.dateFormat = "YYYY-MM-dd HH:mm:ss"
+                let dateString = dateformatter.string(from: currentdate)
+                print(dateString)
+                // 备注：显示你当前操作的时间，格式如上
+                """).font(.system(.body, design: .monospaced)).padding(.bottom)
+                
+                Text("""
+                \(Text("指定的字符串转化为 Date").bold().font(.system(.body, design: .default)))
+                let dateString = "2023-10-30"let dateformatter = DateFormatter()
+                dateformatter.dateFormat = "YYYY-MM-dd"
+                let newDate = dateformatter.date(from: dateString)
+                print(newDate)
+                """).font(.system(.body, design: .monospaced)).padding(.bottom)
+            }
+        }.padding()
     }
 }
